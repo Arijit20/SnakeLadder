556547a1f7bfc2749d39fdaafd3bf4419b6e3099
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-24 13:55:10 +0530

    starting the snake ladder game

---
+++ src/com/cg/SnakeLadderGame.java
@@ -0,0 +1,10 @@
+package com.cg;
+
+public class SnakeLadderGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Starting the game");
+	}
+
+}
