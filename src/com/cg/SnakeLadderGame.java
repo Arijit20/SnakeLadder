8c6fef23f35651e6291ab36b9045acb57fea07ef
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-24 16:15:18 +0530

    no of players is 2

--- src/com/cg/SnakeLadderGame.java
+++ src/com/cg/SnakeLadderGame.java
@@ -12,16 +12,28 @@ public class SnakeLadderGame {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	 int playerCount = 1;
+	 int playerCount = 2;
 	 int currentPosition = 0;
 	 int count = 0;
+	 int playerFlag = 0;
+	 //playerFlag : 0 for 1st player and 1 for 2nd player
+	 int ladderFlag = 0;
+	 //ladderFlag : 1 for ladder and 0 for others
+	 
      System.out.println("Starting the game");
-     System.out.println("No of player : " + playerCount);
-     System.out.println("Initial position of the player :" + STARTING_POSITION);
+     System.out.println("No of players : " + playerCount);
+     System.out.println("Initial position of both the players :" + STARTING_POSITION);
+     System.out.println("Player 1 is first to roll dice");
      
      while(currentPosition != WINNING_POSITION) {
     	 count++;
     	 
+    if(playerFlag == 0)
+    	System.out.println("Playing : Player 1");
+    else
+    	System.out.println("Playing : Player 2");
+    
+    
      Random r = new Random();
      int number_on_Die =  r.nextInt(6) + 1 ;
   
@@ -32,11 +44,14 @@ public class SnakeLadderGame {
      case NO_PLAY:
     	 currentPosition += 0;
     	 break;
+    	 
      case LADDER:
+    	 ladderFlag = 1;
     	 currentPosition += number_on_Die;
     	 if(currentPosition > WINNING_POSITION)
     		 currentPosition -= number_on_Die;
     	 break;
+    	 
     default:{
     	currentPosition -= number_on_Die;
     	if(currentPosition < STARTING_POSITION )
@@ -44,6 +59,21 @@ public class SnakeLadderGame {
     } 
      }
      System.out.println("New Position is : " + currentPosition);
+     
+     if(ladderFlag == 0) {
+    	 if(playerFlag == 0)
+    		 playerFlag = 1;
+    	 else
+    		 playerFlag = 0;
+    	 
+     }
+     
+     else if (ladderFlag == 1) {
+    	 playerFlag += 0;
+    	 ladderFlag = 0;
+     }
+     
+    	 
 	}
      System.out.println("The dice was played " + count +" times");
   }
