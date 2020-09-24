3aa0ccfd5b76d20fe660e0d3cb4e2f41071c253e
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-24 14:01:11 +0530

    statred with single player at position 0

--- src/com/cg/SnakeLadderGame.java
+++ src/com/cg/SnakeLadderGame.java
@@ -2,9 +2,15 @@ package com.cg;
 
 public class SnakeLadderGame {
 
+	public static final int STARTING_POSITION = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 int playerCount = 1;
      System.out.println("Starting the game");
+     System.out.println("No of player : " + playerCount);
+     System.out.println("Initial position of the player :" + STARTING_POSITION);
+     
 	}
 
 }
