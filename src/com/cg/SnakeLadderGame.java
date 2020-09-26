fe31da50c6087ba0145d4a75a0de5521b8e000fb
Arijit20 <71620955+Arijit20@users.noreply.github.com>
2020-09-26 14:10:17 +0530

    Update SnakeLadderGame.java

--- src/com/cg/SnakeLadderGame.java
+++ src/com/cg/SnakeLadderGame.java
@@ -1,84 +1,97 @@
 package com.cg;
+
 import java.util.Random;
 
 public class SnakeLadderGame {
 
-	public static final int STARTING_POSITION = 0;
+	public static final int INITIAL_POSITION = 0;
+	public static final int WINNING_POSITION = 100;
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
-	public static final int WINNING_POSITION = 100;
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	 int playerCount = 2;
-	 int currentPosition = 0;
-	 int count = 0;
-	 int playerFlag = 0;
-	 //playerFlag : 0 for 1st player and 1 for 2nd player
-	 int ladderFlag = 0;
-	 //ladderFlag : 1 for ladder and 0 for others
-	 
-     System.out.println("Starting the game");
-     System.out.println("No of players : " + playerCount);
-     System.out.println("Initial position of both the players :" + STARTING_POSITION);
-     System.out.println("Player 1 is first to roll dice");
-     
-     while(currentPosition != WINNING_POSITION) {
-    	 count++;
-    	 
-    if(playerFlag == 0)
-    	System.out.println("Playing : Player 1");
-    else
-    	System.out.println("Playing : Player 2");
-    
-    
-     Random r = new Random();
-     int number_on_Die =  r.nextInt(6) + 1 ;
-  
-     System.out.println("Number got from rolling the die is "+ number_on_Die);
-     
-     int condition =(int)Math.floor(Math.random() * 10) % 3;
-     switch(condition) {
-     case NO_PLAY:
-    	 currentPosition += 0;
-    	 break;
-    	 
-     case LADDER:
-    	 ladderFlag = 1;
-    	 currentPosition += number_on_Die;
-    	 if(currentPosition > WINNING_POSITION)
-    		 currentPosition -= number_on_Die;
-    	 break;
-    	 
-    default:{
-    	currentPosition -= number_on_Die;
-    	if(currentPosition < STARTING_POSITION )
-    		currentPosition = STARTING_POSITION;
-    } 
-     }
-     System.out.println("New Position is : " + currentPosition);
-     
-     if(ladderFlag == 0) {
-    	 if(playerFlag == 0)
-    		 playerFlag = 1;
-    	 else
-    		 playerFlag = 0;
-    	 
-     }
-     
-     else if (ladderFlag == 1) {
-    	 playerFlag += 0;
-    	 ladderFlag = 0;
-     }
-     
-    	 
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to Snake Ladder Game");
+		System.out.println("No of Players : 2 ");
+		System.out.println("Player 1 is starting the game");
+		int position1 = 0, position2 = 0, count1 = 0, count2 = 0;
+		int playerFlag = 0;
+
+		while ((position1 != WINNING_POSITION) && (position2 != WINNING_POSITION)) {
+
+			Random r = new Random();
+			int numberOnDie = r.nextInt(6) + 1;
+
+			int option = (int) Math.floor(Math.random() * 10) % 3;
+
+			if (playerFlag == 0) {
+				System.out.println("Position of player 1 is :" + position1);
+				count1++;
+				System.out.println("Playing : Player 1");
+				System.out.println("Outcome of Die :" + numberOnDie);
+
+				if(option == NO_PLAY) {
+				    playerFlag  = 1;
+					System.out.println("Position of player 1 is :" + position1);
+				}
+
+				else if(option == NO_PLAY){
+					position1 = +numberOnDie;
+					if (position1 > WINNING_POSITION)
+						position1 = position1 - numberOnDie;
+					System.out.println("Position of player 1 is :" + position1);
+				}
+					
+				else if(option == SNAKE) {
+					playerFlag = 1;
+					position1 -= numberOnDie;
+					if (position1 < INITIAL_POSITION)
+						position1 = INITIAL_POSITION;
+					System.out.println("Position of player 1 is :" + position1);
+				}
+				
+				
+			}
+
+		else if (playerFlag == 1) {
+			System.out.println("Position of player 2 is :" + position2);
+				count2++;
+				System.out.println("Playing : Player 2");
+
+				System.out.println("Outcome of Die :" + numberOnDie);
+
+				if(option == NO_PLAY){
+				    playerFlag = 0;
+					System.out.println("Position of player 2 is :" + position2);
+				}
+
+					else if (option == LADDER) {
+					
+					position2 = +numberOnDie;
+					if (position2 > WINNING_POSITION)
+						position2 = position2 - numberOnDie;
+					System.out.println("Position of player 2 is :" + position2);
+				}
+					
+					else if (option == SNAKE) {
+						playerFlag = 0;
+					position2 -= numberOnDie;
+					if (position2 < INITIAL_POSITION)
+						position2 = INITIAL_POSITION;
+					System.out.println("Position of player 2 is :" + position2);
+				}
+				
+				}
+				
+			}
+			
+			if (playerFlag == 0)
+			System.out.println("Player 2 won the game");
+		else
+			System.out.println("Player 1 won the game");
+		
+		System.out.println("Number of times dice rolled by Player 1 :"+ count1);
+		System.out.println("Number of times dice rolled by Player 2 :"+ count2);
+		}
 	}
-     System.out.println("The dice was played " + count +" times");
-     if(playerFlag == 0)
-    	 System.out.println("1st Player won the game");
-     else
-    	 System.out.println("2nd Player won the game.");
-  }
-}
+
